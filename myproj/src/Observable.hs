c0df339b99acc1a62b90fddbe1a340031152e61c
este78 <araujofe@tcd.ie>
2019-03-30 16:06:41 +0000

    date continuation

--- myproj/src/Observable.hs
+++ myproj/src/Observable.hs
@@ -6,31 +6,41 @@ module Observable where
 
 data Date = Day Int deriving (Show, Eq, Ord)
 
+--Returns the day 
 horizon :: Date -> Int
 horizon (Day a) = a
 
+--Returns the difference between two dates in Int
 dayDiff :: Date -> Date -> Int
 dayDiff (Day a) (Day b) = b - a
 
+--returns the higher date in days
 maxDate :: Date -> Date -> Date
-maxDate (Day a)(Day b) = Day (max a b)
+maxDate (Day a)(Day b) = Day $ max a b
+
+--returns number of months
+dayToMonth :: Date -> Int
+dayToMonth (Day a) = 
+        if a `mod` 30 == 0
+          then a `div` 30 
+          else (a `div` 30) + 1
 
 
 
-
-
-data Obs a where
-  External :: String -> Obs a
-  Constant :: a -> Obs a
-  After :: Date -> Obs Bool
-  Before :: Date -> Obs Bool
-  At :: Date -> Obs Bool
-  OAnd :: Obs Bool -> Obs Bool -> Obs Bool
-  OGreaterThan :: Obs Int -> Obs Int -> Obs Bool
-  OSubtract :: Obs Int -> Obs Int -> Obs Int
+--Code from rossng/merchant github
+--{-# LANGUAGE TypeOperators, MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, GADTs #-}
+-- data Obs a where
+  -- External :: String -> Obs a
+  -- Constant :: a -> Obs a
+  -- After :: Date -> Obs Bool
+  -- Before :: Date -> Obs Bool
+  -- At :: Date -> Obs Bool
+  -- OAnd :: Obs Bool -> Obs Bool -> Obs Bool
+  -- OGreaterThan :: Obs Int -> Obs Int -> Obs Bool
+  -- OSubtract :: Obs Int -> Obs Int -> Obs Int
   
 
-between :: Date -> Date -> Obs Bool
-between t1 t2 = OAnd (After t1) (Before t2)
+-- between :: Date -> Date -> Obs Bool
+-- between t1 t2 = OAnd (After t1) (Before t2)
 
 
