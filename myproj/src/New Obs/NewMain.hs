8128e7a3511af2edeee6fecc5f7f081cbae23e59
este78 <araujofe@tcd.ie>
2019-04-19 14:05:36 +0100

    Rate String ; Date and Day implemented

---
+++ myproj/src/New Obs/NewMain.hs
@@ -0,0 +1,25 @@
+module NewMain where
+
+import NewObs
+
+main :: IO ()
+main =do
+       putStr ("Filename: ")
+       fname <- getLine
+       txt <- readFile fname
+       putStr "continue?"
+       getLine
+       let creads = read txt
+       putStrLn ("Have "++show (length creads)++"(s) contracts")
+       putStr ("Pick=one 0,1,2,...length-1 \n")
+       txt <- getLine
+       let c = creads!!(read txt)
+       putStrLn ("Read:\n"++represent c)
+       main
+
+showcontracts [] = putStrLn "\nDone!"
+showcontracts (c:cs)
+ = do putStrLn ("Read:\n"++represent c)
+      putStr "continue?"
+      getLine
+      showcontracts cs
